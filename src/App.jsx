c66046f11f55dea010cd1758e88ae9a44b07e2e2
Raul Gomez <rauljgomez14@gmail.com>
2022-08-07 18:22:21 -0500

    Añadiendo lo faltante

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import WeatherTemplate from './components/WeatherTemplate'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [location, setLocation] = useState()
 
+    useEffect(() => {
+      const success = pos => {
+       
+          const latlon = {
+            lat: pos.coords.latitude,
+            lon: pos.coords.longitude
+          }
+          setLocation(latlon)
+        }
+    
+      navigator.geolocation.getCurrentPosition(success)
+    }, [])
+
+  console.log(location)
+
+    
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <WeatherTemplate lat={location?.lat} lon={location?.lon} /> 
+
     </div>
   )
 }
---
+++ src/components/ScreenWait.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ScreenWait = () => {
+  return (
+    <div>
+        <div className="container_loader">
+            <div className="loader"></div>
+        </div>
+    </div>
+  )
+}
+
+export default ScreenWait
